b3b84c994497cd42fa4aae69f41cb352687903b5
MaxTynkovich <m_wildfires@mail.ru>
2021-05-12 22:17:40 +0300

    Adding the AddTodo component

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import { Navbar } from './src/NavBar'
+import { AddTodo } from './src/AddTodo'
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Navbar title='Todo App'/>
-      <StatusBar style="auto" />
+    <View>
+      <Navbar title="Todo App" />
+      <View style={styles.container}>
+        <AddTodo />
+      </View>
     </View>
   );
 }
---
+++ src/AddTodo.js
@@ -0,0 +1,46 @@
+import React, {useState} from 'react'
+import {View, Text, StyleSheet, TextInput, Button, Alert} from 'react-native'
+
+export const AddTodo = ({onSubmit}) => {
+    const [value, setValue] = useState('')
+
+    const pressHandler = () => {
+        if(value.trim()){
+        onSubmit(value)
+        setValue('')
+        } else {
+        Alert.alert('Ошибка!')
+    }}
+
+    return (
+        <View style={styles.block}>
+            <TextInput
+             style={styles.input} 
+             placeholder="Название задачи"
+             onChangeText={setValue}
+             value={value}
+             autoCorrect={false}
+             />
+            <Button style={styles.button} title="Добавить" onPress={pressHandler}/>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    block: {
+    marginTop: 55,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    flexDirection: 'row',
+    marginBottom: 15
+    },
+    input: {
+        width: '60%',
+        height: 30,
+        borderBottomColor: 'gray',
+        borderBottomWidth: 2,        
+        textAlign: 'center',
+    },
+    button: {
+    }
+})
